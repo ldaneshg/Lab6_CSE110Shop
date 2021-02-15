06ba3511190f08e097360f34ca20a8e7a7f426e5
Lara Daneshgaran <ldaneshg@ucsd.edu>
2021-02-14 23:59:13 -0800

    Final

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,16 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+    const shadow = this.attachShadow({mode: 'open'});
+    shadow.innerHTML = 
+      '<link rel="stylesheet" href="./styles/styles.css" /><li class="product"><img id="img" src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" ' + 
+      'alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200> ' +
+      '<p class="title" id="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p> <p class="price" id="price">$109.95</p>'
+      + '<button onclick="alert("Added to Cart!")">Add to Cart</button> </li>';
+  }
 }
 
-customElements.define('product-item', ProductItem);
+customElements.define('product-item', ProductItem); 
+
--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,25 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  localStorage.setItem("array", JSON.stringify(response));
+  .then(response => response.json()
+  .then( data => {
 
-  var arr = JSON.parse(localStorage.getItem("array"));
-});
+  //var arr = JSON.parse(localStorage.getItem("array"));
+  
+  data.forEach( function(item, index) {
+    
+    let list = document.getElementById('product-list');
+    window.localStorage.setItem(String(index), JSON.stringify(item)); 
+    let product = document.createElement('product-item');
+    let title = product.shadowRoot.getElementById('title');
+    let img = product.shadowRoot.getElementById('img');
+    let price = product.shadowRoot.getElementById('price');
+    title.innerHTML = item.title; 
+    img.src = item.image;
+    price.innerHTML = '$' + item.price; 
+    list.appendChild(product);
+  });
+  }));
+}); 
+
+
